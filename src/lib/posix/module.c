0e762f237e7b4b63488f32e8b960da0891b64e29
jussi <jussi.kuokkanen@protonmail.com>
2019-09-24 00:54:58 +0300

    add posix library loading functions

--- src/lib/posix/module.c
+++ src/lib/posix/module.c
@@ -6,4 +6,15 @@
 void *tc_dlopen(const char *path) {
     return dlopen(path, RTLD_LAZY);
 }
-        
+
+void *tc_dlsym(void *handle, const char *name) {
+    return dlsym(handle, name);
+}
+
+void tc_dlclose(void *handle) {
+    dlclose(handle);
+}
+
+char *tc_dlerror() {
+    return dlerror();
+}
