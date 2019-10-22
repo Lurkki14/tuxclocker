f8d0dbb7b8ab1aa1f6d65aed8cba3be232b904ef
jussi <jussi.kuokkanen@protonmail.com>
2019-10-22 21:29:16 +0300

    fix index in tc_str_arr_dup

--- src/lib/tc_common.c
+++ src/lib/tc_common.c
@@ -3,11 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-char **tc_str_arr_dup(int8_t str_count, char **const strings) {
+char **tc_str_arr_dup(uint16_t str_count, char **const strings) {
     char **ptr_arr = calloc(str_count, sizeof(char*));
-    for (int8_t i = 0; i < str_count; i++) {
+    for (uint16_t i = 0; i < str_count; i++) {
         ptr_arr[i] = strdup(strings[i]);
     }
     
-    return NULL;
+    return ptr_arr;
+}
+
+void tc_str_arr_free(uint16_t str_count, char **strings) {
+    for (uint16_t i = 0; i < str_count; i++) {
+        free(strings[i]);
+    }
+    free(strings);
 }
