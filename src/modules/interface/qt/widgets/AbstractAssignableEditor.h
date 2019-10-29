156affe8373f0d6e9a9849e2a756bed62e9e5f40
jussi <jussi.kuokkanen@protonmail.com>
2019-10-29 14:25:23 +0200

    add AbstractAssignableEditor as a common interface for assignable editors

---
+++ src/modules/interface/qt/widgets/AbstractAssignableEditor.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// Defines the common interface for assignable editors
+
+#include <QVariant>
+#include <QWidget>
+
+#include <AssignableData.h>
+
+class AbstractAssignableEditor : public QWidget {
+public:
+    AbstractAssignableEditor(QWidget *parent = nullptr) : QWidget(parent) {}
+    virtual void setValue(QVariant value) = 0;
+    virtual void setAssignableData(const AssignableData &data) = 0;
+    virtual QVariant value() = 0;
+};
