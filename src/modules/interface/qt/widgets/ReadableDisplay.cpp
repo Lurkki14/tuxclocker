5b9c0b2cc44dafcdc1ac5baeb2520ed62724a1b3
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2019-11-17 15:17:39 +0200

    qt: update ReadableDisplay

--- src/modules/interface/qt/widgets/ReadableDisplay.cpp
+++ src/modules/interface/qt/widgets/ReadableDisplay.cpp
@@ -1,4 +1,21 @@
-#include "ReadableDisplay.h"
+ #include "ReadableDisplay.h"
 
 ReadableDisplay::ReadableDisplay(QWidget *parent) : QWidget(parent) {
+    setAcceptDrops(true);
+    
+    m_displayWidgetLayout = new QVBoxLayout;
+    
+    m_displayWidgetLayout->addWidget(new ReadableGraphDisplay);
+    
+    setLayout(m_displayWidgetLayout);
+}
+
+void ReadableDisplay::dropEvent(QDropEvent *event) {
+}
+
+void ReadableDisplay::dragEnterEvent(QDragEnterEvent *event) {
+    if (!event->mimeData()->hasFormat(ReadableData::mimeType())) {
+        return;
+    }
+    event->accept();
 }
