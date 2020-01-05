35d99f54f2f86a6d1c664d92d1d28610ecf2ddc5
jussi <jussi.kuokkanen@protonmail.com>
2020-01-05 17:19:31 +0200

    daemon: check for NULL assign callback in AssignableAdaptorFactory

--- src/tuxclockerd/AssignableAdaptorFactory.cpp
+++ src/tuxclockerd/AssignableAdaptorFactory.cpp
@@ -6,11 +6,15 @@
 #include <QDebug>
 
 QDBusAbstractAdaptor *AssignableAdaptorFactory::assignableAdaptor(QObject *parent, const tc_assignable_node_t *node) {
+	if (!node->assign_callback) {
+		// Placeholder node
+		return nullptr;
+	}
+	
 	switch (node->value_category) {
 		case TC_ASSIGNABLE_RANGE:
 			return new AssignableVariantRangeAdaptor(parent, node);
 		case TC_ASSIGNABLE_ENUM:
-			qDebug() << "using enum adaptor";
 			return new AssignableEnumAdaptor(parent, node);
 		default:
 			return nullptr;
