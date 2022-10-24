ce1240226d79e44ef84df5f7064e02315f4e1ce7
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2022-10-24 16:40:03 +0300

    comment on working boost version (1.79.0)

--- release.nix
+++ release.nix
@@ -2,4 +2,6 @@ with import <nixpkgs> {};
 libsForQt5.callPackage ./default.nix {
   libXNVCtrl = linuxPackages.nvidia_x11.settings.libXNVCtrl;
   nvidia_x11 = linuxPackages.nvidia_x11;
+  #boost = boost179;
+  # TODO: I'd like to pin this, but specifying 1.79 gives a compile error even though the exact same hash is used if 'boost' is used
 }
