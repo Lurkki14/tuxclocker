ec368cd76ef6c600eb20026c6ca2cd65c53ac650
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-09-16 17:05:16 +0300

    update nix expression

--- default.nix
+++ default.nix
@@ -26,12 +26,8 @@ mkDerivation rec {
     fetchSubmodules = true;
     owner = "Lurkki14";
     repo = "tuxclocker";
-    rev = "2557227170cb94be12008bced6b9b3cfd93b8342";
-    sha256 = "0gncf65hy11zwjf7mpz4k57ik15980mssykj2r1zd6gda48pw832";
-    #rev = "002c2868118b2fa7d2e8db9d8a84a6b47dd261e5";
-    #rev = "b0c39bfaeb1b5da20accd0d65dacfd5ac13a6225";
-    #sha256 = "02jribnk9d9h1y01fbl9pafyz4vybfkma2mv73n7i707gfv6dk2r";
-    #sha256 = "0a553i6mr9hxlfzvgidsn0ss0hbcr9jdiaknhv59bailalajn58i";
+    rev = "28ce25736bfeacd28a591c8fc98efaa0bf5e5063";
+    hash = "sha256-Y91mUbontxomE9fZWP5FLblRIDgwN6nO16HevV27Fxc=";
   };
 
   # meson 0.57 should fix having to have these
