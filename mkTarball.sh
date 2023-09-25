1bda9194d86b12f029def31a6f14a16fa6b0e758
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-09-25 17:59:16 +0300

    fix conflict

--- mkTarball.sh
+++ mkTarball.sh
@@ -3,8 +3,6 @@
 storePaths=($(nix-store -qR $(nix-build release.nix)))
 cp -n result/bin/{.tuxclockerd-wrapped,.tuxclocker-qt-wrapped} .
 libPaths=( "${storePaths[@]/%/\/lib\/}" )
-<<<<<<< HEAD
-<<<<<<< HEAD
 libPaths=( "${libPaths[@]/#/.}" )
 libPathsColonSep=$(echo ${libPaths[@]} | sed 's/ /:/g')
 glibPath=$(nix-build '<nixpkgs>' -A glibc --no-out-link)
@@ -28,26 +26,4 @@ sudo kill \$(pidof .tuxclockerd-wrapped)
 " > run.sh
 chmod +x run.sh
 tar cavf tuxclocker.tar ${storePaths[@]} ./.tuxclocker-qt-wrapped ./.tuxclockerd-wrapped ./run.sh ./dev/dbusconf.conf
-=======
-#libPaths=( "${libPaths[@]/" "/\:}" )
-=======
->>>>>>> 641bf86 (squash: working ld paths)
-libPaths=( "${libPaths[@]/#/.}" )
-libPathsColonSep=$(echo ${libPaths[@]} | sed 's/ /:/g')
-glibPath=$(nix-build '<nixpkgs>' -A glibc --no-out-link)
-chmod 777 ./.tuxclockerd-wrapped ./.tuxclocker-qt-wrapped
-patchelf --set-rpath \.$glibPath/lib ./.tuxclockerd-wrapped ./.tuxclocker-qt-wrapped
-patchelf --set-interpreter \.$glibPath/lib/ld-linux-x86-64.so.2 ./.tuxclockerd-wrapped ./.tuxclocker-qt-wrapped
 
-echo "
-export DBUS_SYSTEM_BUS_ADDRESS='unix:path=/tmp/tuxclocker-dbus-socket'
-export LD_LIBRARY_PATH=\"${libPathsColonSep[@]}\"
-sudo -E LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH\" dbus-run-session --config-file=dev/dbusconf.conf ./.tuxclockerd-wrapped & ./.tuxclocker-qt-wrapped
-" > run.sh
-chmod +x run.sh
-<<<<<<< HEAD
-#tar cavf foo.tar ${storePaths[@]} ./.tuxclocker-qt-wrapped ./.tuxclockerd-wrapped ./run.sh ./dev/dbusconf.conf
->>>>>>> 397cfb9 (squash: non-nix-bundle)
-=======
-tar cavf foo.tar ${storePaths[@]} ./.tuxclocker-qt-wrapped ./.tuxclockerd-wrapped ./run.sh ./dev/dbusconf.conf
->>>>>>> 641bf86 (squash: working ld paths)
