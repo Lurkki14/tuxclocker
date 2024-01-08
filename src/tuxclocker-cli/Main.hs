7b27625e79a448f7da3663625b115a9260c2afaa
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2024-01-08 18:08:12 +0200

    show DynamicReadable values

--- src/tuxclocker-cli/Main.hs
+++ src/tuxclocker-cli/Main.hs
@@ -7,17 +7,21 @@ import Data.Text (Text)
 import Data.Tree
 import DBus
 import DBus.Client
+import qualified DBus.Internal.Types as I
 import qualified DBus.Introspection as I
+
+data DeviceInterface =
+  DynamicReadable
+
+data DeviceNode = DeviceNode {
+  object :: I.Object,
+  interface :: Maybe DeviceInterface
+}
+
+newtype DynamicReadableNode = DynamicReadableNode ObjectPath
 
 tuxClockerCall :: MethodCall -> MethodCall
 tuxClockerCall call = call { methodCallDestination = Just "org.tuxclocker" }
-
-printTree :: Show a => Tree a -> IO ()
-printTree tree = printIndent tree 0 where
-  printIndent :: Show a => Tree a -> Int -> IO ()
-  printIndent (Node rootLabel subForest) level = do
-    print $ (replicate level ' ') <> show rootLabel
-    mapM_ (\tree -> printIndent tree (level + 1)) subForest
 
 getObject :: Client -> ObjectPath -> IO I.Object
 getObject client path = do
@@ -28,6 +32,28 @@ getObject client path = do
     (error ("Invalid introspection XML: " ++ show xml))
     (xml >>= I.parseXML path)
 
+deviceInterface :: I.Object -> Maybe DeviceInterface
+deviceInterface object =
+  go where
+    interfaceNames = I.interfaceName <$> I.objectInterfaces object
+    hasInterface :: I.InterfaceName -> Bool
+    hasInterface str = elem str interfaceNames
+    go
+      | hasInterface "org.tuxclocker.DynamicReadable" = Just DynamicReadable
+      | otherwise = Nothing
+
+getShowNode :: Client -> DeviceNode -> IO String
+getShowNode client node = do
+  name <- getName client $ I.objectPath $ object node
+  ifaceText <- getShowInterface client node
+  pure $ name <> "\n" <> ifaceText
+
+getShowInterface :: Client -> DeviceNode -> IO String
+getShowInterface client node = go client node where
+  go client node@DeviceNode { interface = Just DynamicReadable } =
+    getValue client $ DynamicReadableNode $ I.objectPath $ object node
+  go _ _ = pure ""
+
 getName :: Client -> ObjectPath -> IO String
 getName client path =
   let
@@ -35,23 +61,31 @@ getName client path =
   in
     getProperty client call <&> fromRight (toVariant ("Unnamed" :: String)) <&> variantToString
 
-getDBusTree :: Client -> IO (Tree Variant)
+getValue :: Client -> DynamicReadableNode -> IO String
+getValue client (DynamicReadableNode path) =
+  let
+    _call = tuxClockerCall $ methodCall path "org.tuxclocker.DynamicReadable" "value"
+  in do
+    reply <- call_ client _call
+    pure $ show $ head $ methodReturnBody reply
+
+getDBusTree :: Client -> IO (Tree I.Object)
 getDBusTree client = unfoldTreeM (buildNode client) "/" where
-  buildNode :: Client -> ObjectPath -> IO (ObjectPath, [ObjectPath])
+  buildNode :: Client -> ObjectPath -> IO (I.Object, [ObjectPath])
   buildNode client path = do
     object <- getObject client path
     let childPaths = I.objectPath <$> I.objectChildren object
-    pure (I.objectPath object, childPaths)
+    pure (object, childPaths)
 
 variantToString :: Variant -> String
 variantToString x = fromMaybe "Invalid" $ fromVariant x
 
-getNameTree :: Client -> Tree ObjectPath -> IO (Tree String)
-getNameTree client = mapM (getName client)
-
+getShowTree :: Client -> Tree I.Object -> IO (Tree String)
+getShowTree client =
+  mapM (\obj -> getShowNode client $ DeviceNode obj $ deviceInterface obj)
 
 main = do
   client <- connectSystem
   tree <- getDBusTree client
-  nameTree <- getNameTree client tree
-  putStr $ drawTree nameTree
+  treeShow <- getShowTree client tree
+  putStr $ drawTree treeShow
